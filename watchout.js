7691da2f0c3391e0c45e43d44ac3e5f8b49e0c20
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-25 15:33:16 -0700

    Added enemies to playingField. Enemies start in random positions

--- watchout.js
+++ watchout.js
@@ -4,7 +4,29 @@ var gameOptions = {};
 
 gameOptions.height = 768;
 gameOptions.width = 1024;
+gameOptions.enemies = 30;
 
 d3.select('body').append('svg')
+.attr('class', 'playingField')
 .attr('height', gameOptions.height)
 .attr('width', gameOptions.width);
+
+var playingField  = d3.select('svg');
+
+var enemies = new Array(gameOptions.enemies);
+
+// Initialize enemies positions
+for (var i = 0; i < enemies.length; i++) {
+  var x = Math.floor(Math.random() * gameOptions.width);
+  var y = Math.floor(Math.random() * gameOptions.height);
+  enemies[i] = [x, y];
+}
+
+// Insert enemies into playing field
+playingField.selectAll('circle')
+.data(enemies)
+.enter()
+.append('circle')
+.attr('cx', function(d) {return d[0];})
+.attr('cy', function(d) {return d[1];})
+.attr('r', 10);
