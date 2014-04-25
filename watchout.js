515625b7ea24a1eb33eda0e39686964a0ed55713
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-25 15:09:06 -0700

    Added playing field

--- watchout.js
+++ watchout.js
@@ -1,2 +1,10 @@
 // start slingin' some d3 here.
 
+var gameOptions = {};
+
+gameOptions.height = 768;
+gameOptions.width = 1024;
+
+d3.select('body').append('svg')
+.attr('height', gameOptions.height)
+.attr('width', gameOptions.width);
