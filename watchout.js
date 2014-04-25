b888e961f74eabb373089c7f53b6d5b6250ef063
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-25 15:40:27 -0700

    Enemies now move to new random position every second

--- watchout.js
+++ watchout.js
@@ -4,7 +4,7 @@ var gameOptions = {};
 
 gameOptions.height = 768;
 gameOptions.width = 1024;
-gameOptions.enemies = 30;
+gameOptions.enemies = 300;
 
 d3.select('body').append('svg')
 .attr('class', 'playingField')
@@ -16,11 +16,14 @@ var playingField  = d3.select('svg');
 var enemies = new Array(gameOptions.enemies);
 
 // Initialize enemies positions
-for (var i = 0; i < enemies.length; i++) {
-  var x = Math.floor(Math.random() * gameOptions.width);
-  var y = Math.floor(Math.random() * gameOptions.height);
-  enemies[i] = [x, y];
-}
+var setRandomEnemyPosition = function() {
+  for (var i = 0; i < enemies.length; i++) {
+    var x = Math.floor(Math.random() * gameOptions.width);
+    var y = Math.floor(Math.random() * gameOptions.height);
+    enemies[i] = [x, y];
+  }
+};
+setRandomEnemyPosition();
 
 // Insert enemies into playing field
 playingField.selectAll('circle')
@@ -30,3 +33,15 @@ playingField.selectAll('circle')
 .attr('cx', function(d) {return d[0];})
 .attr('cy', function(d) {return d[1];})
 .attr('r', 10);
+
+// Have enemies move every second
+setInterval(function() {
+  setRandomEnemyPosition();
+  playingField.selectAll('circle')
+  .data(enemies)
+  .transition()
+  .duration(500)
+  .attr('cx', function(d) {return d[0];})
+  .attr('cy', function(d) {return d[1];})
+  .attr('r', 10);
+},1000);
